63b028ac47f6fecad11153896756ed79d118773a
SANDARA\Sandra <a25660@svalero.com>
2023-06-14 22:33:29 +0200

    footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,9 @@
 <!--Footer-->
 <template>
   <div class="footer" style="margin-top: 30px;">
+    <p>Política de Privacidad</p>
     <p>Api create by Sandra</p>
+    <p>Aviso Legal y Cookies</p>
   </div>
 </template>
 
@@ -13,11 +15,20 @@
 </script>
 <style>
   .footer{
-    background-color:lightgrey;  
     background-color: lightgrey;
     display: flex;
-    justify-content: center;
-    height: 50px;
+    justify-content: space-around;
+    height: auto;
     align-items: center;
+    flex-direction: row;
+    padding-top: 20px;
+    padding-bottom: 20px;
 }
+  .footer:hover{
+    background-color: lightgrey;
+    color: slateblue;
+    font-weight: 600;
+    cursor: pointer;
+}
+
 </style>
